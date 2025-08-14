3249c78b9e868d7c97bb14207eeb1c99b5a8005b
dudaclaudino7 <rodrigues.santos.maria2701@escola.pr.gov.br>
2025-08-14 14:41:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao( 
+ 'História' ,
+ 'Qual civilização antiga construiu a pirâmide de Gizé?',
+ 'Egípcia'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual é o maior oceano do mundo?',
+  'Oceano Pacífico'
+)
+
+criaCartao(
+  'Ciência',
+  'Qual elemento químico tem o símbolo "O"?',
+  'Oxigênio'
+)
+
+criaCartao(
+  'Literatura',
+  'Quem escreveu a obra "Dom Quixote"?',
+  'Miguel de Cervantes'
+)
+
+criaCartao(
+  'Arte',
+  'Qual pintor é conhecido por sua obra "A Noite Estrelada"?',
+  'Vincent van Gogh'
+)
+
+criaCartao(
+  'Música',
+  'Qual banda ficou famosa com a música "Bohemian Rhapsody"?',
+  'Queen'
+)
+
+criaCartao(
+  'Esportes',
+  'Em que ano o Brasil ganhou sua primeira Copa do Mundo?',
+  '1958'
+)
+
+criaCartao(
+  'História',
+  'Quem foi o primeiro presidente dos Estados Unidos?',
+  'George Washington'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual é a capital da Austrália?',
+  'Canberra'
+)
